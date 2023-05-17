84491b0e9b0b16778c740aa922f77a4bea08a3a1
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-05-18 04:13:05 +0600

    Footer is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./Pages/Shared/Header/Header";
+import Footer from "./Pages/Shared/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
