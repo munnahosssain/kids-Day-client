687c342433e31f471244e8b758a8cf34e212e66b
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-05-18 12:35:29 +0600

    private route created

---
+++ src/Pages/Shared/Loading/Loading.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { CirclesWithBar } from "react-loader-spinner";
+
+const Loading = () => {
+  return (
+    <CirclesWithBar
+      height="100"
+      width="100"
+      color="#4fa94d"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      outerCircleColor=""
+      innerCircleColor=""
+      barColor=""
+      ariaLabel="circles-with-bar-loading"
+    />
+  );
+};
+
+export default Loading;
---
+++ src/route/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import Loading from "../Pages/Shared/Loading/loading";
+import { Navigate, useNavigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useNavigate();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ form: location }} replace />;
+};
+
+export default PrivateRoute;
